a4f94c5dc3278c71589a2ee02f5ea18d5d4d3b58
Satria Alipatullah <satrialipatullah@gmail.com>
2023-12-30 22:42:41 +0700

    add product route and updatte user route

--- src/app/api/product/route.ts
+++ src/app/api/product/route.ts
@@ -1,6 +1,13 @@
 import { getAllProducts } from "@/lib/product/getAllProduct";
+import getProductByategoryName from "@/lib/product/getProductByCategoryName";
 
 export async function GET(request: Request) {
   const products = await getAllProducts();
+  return Response.json(products);
+}
+export async function POST(request: Request) {
+  const body = await request.json();
+  console.log({ body: body });
+  const products = await getProductByategoryName(body.categoryName);
   return Response.json(products);
 }
--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -1,6 +1,12 @@
-import prisma from "@/lib/prisma";
+import { getUserById } from "@/lib/user/getUserById";
 
 export async function GET(request: Request) {
-  const users = await prisma.user.findMany();
-  return Response.json({ users });
+  const userById = await getUserById("clqrxai770000hkp0ae7athe7");
+  return Response.json(
+    { message: "success", data: userById },
+    {
+      status: 200,
+      statusText: "OK",
+    },
+  );
 }
