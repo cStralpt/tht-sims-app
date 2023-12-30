1f316fdf0cb3f98d98dc4a9f5d67a265aab2467d
Satria Alipatullah <satrialipatullah@gmail.com>
2023-12-30 22:43:12 +0700

    remove unused

--- src/app/(protected)/page.tsx
+++ src/app/(protected)/page.tsx
@@ -1,7 +1,4 @@
 "use client";
-import { Counter } from "@/components/Counter";
-import Link from "next/link";
-import { useState } from "react";
 
 export default function Home() {
   return <section className="grow border-4">sid</section>;
