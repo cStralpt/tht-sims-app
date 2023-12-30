0623ba3d30b3e3ef5e5d15318ab93957ad222699
Satria Alipatullah <satrialipatullah@gmail.com>
2023-12-30 22:43:46 +0700

    add loading state and render data from database

--- src/app/(protected)/profile/page.tsx
+++ src/app/(protected)/profile/page.tsx
@@ -1,34 +1,86 @@
-import { Input } from "@nextui-org/react";
+"use client";
+import { Input, Spinner } from "@nextui-org/react";
 import Image from "next/image";
+import { useEffect, useState } from "react";
+
+interface UserData {
+  message: string;
+  data: {
+    id: string;
+    name: string;
+    email: string;
+    password: string;
+    avatar: string;
+    position: string;
+    createdAt: string;
+    updatedAt: string;
+  };
+}
 
 export default function Profile() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [getUser, setUser] = useState<UserData | null>(null);
+
+  useEffect(() => {
+    const user = async () => {
+      const fetchUserByid = await fetch("/api/user");
+      const userDataById: UserData = await fetchUserByid.json();
+      setIsLoading(false);
+      setUser(userDataById);
+    };
+    user();
+  }, []);
   return (
     <main className="grow p-16">
       <div className="">
         <div className="">
-          <Image
-            src="/assets/user-photo.png"
-            width={200}
-            height={200}
-            alt="photo pengguna"
-            className="aspect-auto"
-          />
+          {isLoading ? (
+            <Spinner
+              label="Memuat profil..."
+              color="warning"
+              labelColor="warning"
+            />
+          ) : (
+            <Image
+              src={`/assets/${getUser?.data.avatar}`}
+              width={200}
+              height={200}
+              alt="photo pengguna"
+              className="aspect-auto"
+            />
+          )}
         </div>
-        <b className="text-purple-950 text-2xl">Kristanto Wibowo</b>
+        {isLoading ? (
+          <Spinner label="Memuat nama..." color="danger" labelColor="danger" />
+        ) : (
+          <b className="text-purple-950 text-2xl">{getUser?.data.name}</b>
+        )}
       </div>
       <div className="flex gap-4">
-        <Input
-          type="text"
-          value="Kristanto Wibowo"
-          color="secondary"
-          label="Nama Kandidat"
-        />
-        <Input
-          type="text"
-          value="Web Programmer"
-          color="secondary"
-          label="Posisi Kandidat"
-        />
+        {isLoading ? (
+          <Spinner label="Memuat..." color="success" labelColor="success" />
+        ) : (
+          <Input
+            type="text"
+            value={getUser?.data.name}
+            color="secondary"
+            label="Nama Kandidat"
+          />
+        )}
+        {isLoading ? (
+          <Spinner
+            label="Memuat posisi kandidat"
+            color="secondary"
+            labelColor="secondary"
+          />
+        ) : (
+          <Input
+            type="text"
+            value={getUser?.data.position}
+            color="secondary"
+            label="Posisi Kandidat"
+          />
+        )}
       </div>
     </main>
   );
