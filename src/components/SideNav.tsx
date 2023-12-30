78e9fead375359b138ee7c72513eba313e1d496f
Satria Alipatullah <satrialipatullah@gmail.com>
2023-12-30 10:19:40 +0700

    add navigation routes and icon

--- src/components/SideNav.tsx
+++ src/components/SideNav.tsx
@@ -36,19 +36,58 @@ export default function SideNav() {
       <nav className="flex flex-col gap-4">
         {[
           {
-            route: "/",
+            route: "/product",
             name: "Produk",
+            icon: (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="rgba(134, 75, 187, 1)"
+              >
+                <path d="m21.704 5.29-2.997-2.997A.996.996 0 0 0 18 2H6a.996.996 0 0 0-.707.293L2.296 5.29A.994.994 0 0 0 2 5.999V19a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V5.999a.994.994 0 0 0-.296-.709zM6.414 4h11.172l1 1H5.414l1-1zM17 13v1H7v-4h2v2h6v-2h2v3z"></path>
+              </svg>
+            ),
           },
           {
-            route: "/",
+            route: "/profile",
             name: "Profil",
+            icon: (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                fill="rgba(134, 75, 187, 1)"
+                viewBox="0 0 24 24"
+              >
+                <path d="M7.5 6.5C7.5 8.981 9.519 11 12 11s4.5-2.019 4.5-4.5S14.481 2 12 2 7.5 4.019 7.5 6.5zM20 21h1v-1c0-3.859-3.141-7-7-7h-4c-3.86 0-7 3.141-7 7v1h17z"></path>
+              </svg>
+            ),
           },
           {
             route: "/",
             name: "Logout",
+            icon: (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                fill="rgba(134, 75, 187, 1)"
+                height="24"
+                viewBox="0 0 24 24"
+              >
+                <path d="m2 12 5 4v-3h9v-2H7V8z"></path>
+                <path d="M13.001 2.999a8.938 8.938 0 0 0-6.364 2.637L8.051 7.05c1.322-1.322 3.08-2.051 4.95-2.051s3.628.729 4.95 2.051 2.051 3.08 2.051 4.95-.729 3.628-2.051 4.95-3.08 2.051-4.95 2.051-3.628-.729-4.95-2.051l-1.414 1.414c1.699 1.7 3.959 2.637 6.364 2.637s4.665-.937 6.364-2.637c1.7-1.699 2.637-3.959 2.637-6.364s-.937-4.665-2.637-6.364a8.938 8.938 0 0 0-6.364-2.637z"></path>
+              </svg>
+            ),
           },
         ].map((d) => (
-          <Link href={d.route} key={d.name} className="p-4 text-purple-800">
+          <Link
+            href={d.route}
+            key={d.name}
+            className="p-4 text-purple-800 flex gap-2"
+          >
+            {d.icon}
             {d.name}
           </Link>
         ))}
