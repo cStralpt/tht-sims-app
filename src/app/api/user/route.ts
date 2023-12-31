a6ca6a45b489b5643f41c1a2e638b841e7ead17e
Satria Alipatullah <satrialipatullah@gmail.com>
2024-01-01 01:23:28 +0700

    protect user route using jwt

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -1,12 +1,21 @@
 import { getUserById } from "@/lib/user/getUserById";
+import useUserFromJwt from "@/hook/user/useUserFromJwt";
 
-export async function GET(request: Request) {
-  const userById = await getUserById("clqrxai770000hkp0ae7athe7");
-  return Response.json(
-    { message: "success", data: userById },
-    {
-      status: 200,
-      statusText: "OK",
-    },
-  );
+export async function GET() {
+  const { userId, isUserAuthenticated } = useUserFromJwt();
+  if (isUserAuthenticated) {
+    const userById = await getUserById(userId);
+    return Response.json(
+      { message: "success", data: userById },
+      {
+        status: 200,
+        statusText: "OK",
+      },
+    );
+  } else {
+    return Response.json(
+      { message: "heyy!" },
+      { status: 401, statusText: "Unauthorized" },
+    );
+  }
 }
